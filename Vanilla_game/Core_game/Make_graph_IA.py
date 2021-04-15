9a18b000d34a5343bc3b366a59e37d1e4036f001
Nathanesteve <estevenathan.a@gmail.com>
2021-04-15 20:31:34 +0200

    cosmit

--- Vanilla_game/Core_game/Make_graph_IA.py
+++ Vanilla_game/Core_game/Make_graph_IA.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-just_plot = False
+just_plot = True
 
 
+# DONT RUN 
 
 if just_plot is False:
 
@@ -15,7 +16,7 @@ if just_plot is False:
     stockage_score_clockwise = []
     stockage_maxcell_random = []
     stockage_maxcell_clockwise = []
-    for i in range(300):
+    for i in range(1):
         AI_one = Main.Game_2048()
         AI_one.random_2048()
         AI_one.maxcell_find()
@@ -33,63 +34,78 @@ if just_plot is False:
     data_random = np.array(stockage_score_random)
     data_clockwise = np.array(stockage_score_clockwise)
 
-    np.savetxt('Vanilla_game/Data_storage/Storage_AI_score_random.txt', data_random)
-    np.savetxt('Vanilla_game/Data_storage/Storage_AI_score_clockwise.txt', data_clockwise)
-    np.savetxt('Vanilla_game/Data_storage/Storage_AI_maxcell_random.txt', stockage_maxcell_random)
-    np.savetxt('Vanilla_game/Data_storage/Storage_AI_maxcell_clockwise.txt',  stockage_maxcell_clockwise)
+    np.savetxt('Storage_AI_score_random.txt', data_random)
+    np.savetxt('Storage_AI_score_clockwise.txt', data_clockwise)
+    np.savetxt('Storage_AI_maxcell_random.txt', stockage_maxcell_random)
+    np.savetxt('Storage_AI_maxcell_clockwise.txt',  stockage_maxcell_clockwise)
 
 
 
 else:
 
-    data_score_random = np.loadtxt('Vanilla_game/Data_storage/Storage_AI_score_random.txt')
-    data_score_clockwise = np.loadtxt('Vanilla_game/Data_storage/Storage_AI_score_clockwise.txt')
+
+    random_color = '#8A2BE2'
+    clockwise_color = '#FF8C00'
+    data_score_random = np.loadtxt('Storage_AI_score_random.txt')
+    data_score_clockwise = np.loadtxt('Storage_AI_score_clockwise.txt')
 
     Mean_storage_random = (1/np.arange(1, len(data_score_random)+1)) * np.cumsum(data_score_random)
     Mean_storage_clockwise = (1/np.arange(1, len(data_score_clockwise)+1)) * np.cumsum(data_score_clockwise)
 
+
+
+    empirical_mean_random = Mean_storage_random[len(Mean_storage_random) - 1]
+    empirical_mean_clockwise = Mean_storage_clockwise[len(Mean_storage_clockwise) - 1]
+
+    print(f'Empicial mean for random strategie is {empirical_mean_random}')
+    print(f'Empicial mean for random clockwise is {empirical_mean_clockwise}')
+
     plt.figure()
-    plt.plot(Mean_storage_random, color='#FF8C00', label='Random movement')
-    plt.plot(Mean_storage_clockwise, color='#DC143C', label='Clockwise movement')
+    plt.plot(Mean_storage_random, color=random_color, label='Random movement')
+    plt.plot(Mean_storage_clockwise, color=clockwise_color, label='Clockwise movement')
     plt.title("Empirical mean convergence")
     plt.legend()
     plt.xlabel("Number of data")
     plt.ylabel("Empirical mean")
     plt.xlim(0, 1500)
-    plt.ylim(700,1300)
-    plt.savefig('Vanilla_game/Data_storage/AI_score.pdf')
+    plt.ylim(0,4000)
+    plt.savefig('AI_score.pdf')
     plt.show()
 
 
     d = {'Random strategie': data_score_random, 'Clockwise strategie': data_score_clockwise}
     df = pd.DataFrame(d)
     plt.figure()
-    sns.kdeplot(data_score_random, bw_adjust=0.1, legend=False)
-    sns.kdeplot(data_score_clockwise, bw_adjust=0.1)
+    sns.kdeplot(data_score_random, bw_adjust=0.1, legend=True, color=random_color)
+    sns.kdeplot(data_score_clockwise, bw_adjust=0.1, legend=True, color=clockwise_color)
+    # plt.vlines(empirical_mean_random, 0, 0.00125, colors='black')
+    # plt.vlines(empirical_mean_clockwise, 0, 0.00125, colors='black')
+    plt.legend(labels=['Random strategie','Clockwise strategie'])
     plt.title("Distribution of score with random et clockwise strategies")
     plt.xlabel("score")
-    plt.savefig('Vanilla_game/Data_storage/AI_distribution.pdf')
+    plt.savefig('AI_distribution.pdf')
 
 
     plt.show()
 
 
-    data_maxcell_random = np.loadtxt('Vanilla_game/Data_storage/Storage_AI_maxcell_random.txt')
-    data_maxcell_clockwise = np.loadtxt('Vanilla_game/Data_storage/Storage_AI_maxcell_clockwise.txt')
+    data_maxcell_random = np.loadtxt('Storage_AI_maxcell_random.txt')
+    data_maxcell_clockwise = np.loadtxt('Storage_AI_maxcell_clockwise.txt')
    
 
     plt.figure(1)
     
-    plt.hist([data_maxcell_random, data_maxcell_clockwise], label=['Random strategie', 'Clockwise strategie'])
+    plt.hist(data_maxcell_random, label='Random strategie', bins=15, color=random_color)
+    plt.hist(data_maxcell_clockwise, label='Clockwise strategie', bins=15, color=clockwise_color)
     plt.legend(loc='upper right')
     plt.title("Distribution of max score")
     plt.ylabel("Number of apparition among 300 000 try")
     plt.xlabel("Max cell")
-    plt.savefig('Vanilla_game/Data_storage/AI_maxcell.pdf')
+    plt.savefig('AI_maxcell.pdf')
     plt.show()
+    
 
-
-
+    
 
 
 
